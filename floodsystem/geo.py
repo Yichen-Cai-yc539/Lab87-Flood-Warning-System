ff9b660a7ce7e7b9f326a105f1b04120252e6685
mattjfrancis <mf750@cam.ac.uk>
2022-02-10 14:38:07 +0000

    Test 4

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -81,7 +81,7 @@ def rivers_with_station(stations):
 def stations_by_river(stations):
     rivers_stations_of_dict = {}
     for rivers in rivers_with_station(stations):
-        rivers_stations_of_dict[rivers] = sorted([station.name for station in stations if stations.river == rivers])
+        rivers_stations_of_dict[rivers] = sorted([station.name for station in stations if stations.rivers == rivers])
     return rivers_stations_of_dict
     print('hffh')
 
