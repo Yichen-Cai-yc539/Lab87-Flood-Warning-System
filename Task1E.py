31539f2dcaaa55be940a90055d457a3a9b1581db
mattjfrancis <mf750@cam.ac.uk>
2022-02-10 14:45:45 +0000

    Test 69

--- Task1E.py
+++ Task1E.py
@@ -1,5 +1,5 @@
 from floodsystem.stationdata import build_station_list
-from floodsystem.geo import rivers_by_station_data, rivers_by_station_number
+from floodsystem.geo import rivers_by_station_number
 
 def run():
     stations = build_station_list()
