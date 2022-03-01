1f39b88bced7ca9fc7a866379ccd3e2bfe7db2ce
mattjfrancis <mf750@cam.ac.uk>
2022-03-01 22:11:35 +0000

    2F partly and 2G

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -4,16 +4,28 @@ from floodsystem.stationdata import *
 
 
 def polyfit(dates, levels, p):
+
     p_coeff = np.polyfit(dates, levels, p)
-    poly = np.ploy1d(p_coeff)
+    poly = np.poly1d(p_coeff)
 
     return poly, dates[-1]
 
-def relative_risk(station:
-    ## This returns a numerical value depending on how vunerable a station is to flooding
+
+def relative_risk(station):
+    ## Returns a numerical value for the relative risk from flooding for a particular station
+    
     number = 0
     relative_level = MonitoringStation.relative_water_level(station)
     if relative_level == None:
         number = None
-    else: 
-        if relative_level
+    else:
+        if relative_level > 0.5:
+            number += 1
+        if relative_level > 1:
+            number += 1
+        if relative_level > 1.5:
+            number+= 1
+        if relative_level > 2:
+            number +=1
+
+    return number
