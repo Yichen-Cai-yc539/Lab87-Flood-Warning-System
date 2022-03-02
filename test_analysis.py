98d101895e4929e9130a8023639717479b8cec46
mattjfrancis <mf750@cam.ac.uk>
2022-03-02 12:43:44 +0000

    Test ff

--- test_analysis.py
+++ test_analysis.py
@@ -1,3 +1,4 @@
+from xml.dom.expatbuilder import parseString
 from floodsystem.analysis import relative_risk, polyfit
 from floodsystem.stationdata import build_station_list
 
@@ -5,6 +6,5 @@ from floodsystem.stationdata import build_station_list
 
 def test_call():
     station = build_station_list()
-    a = relative_risk(station)
     pass
 
