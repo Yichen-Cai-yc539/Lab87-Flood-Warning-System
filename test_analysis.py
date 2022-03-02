d37da83c93efb27aad7b1562fbe8140d2128d296
mattjfrancis <mf750@cam.ac.uk>
2022-03-02 10:56:16 +0000

    Test redo

--- test_analysis.py
+++ test_analysis.py
@@ -1,6 +1,6 @@
 import floodsystem.analysis 
     
-station = build_station_list()
+
 def test_call(station):
-    a = floodsystem.analysis.relative_risk(station)
+    a = floodsystem.analysis.relative_risk()
 
