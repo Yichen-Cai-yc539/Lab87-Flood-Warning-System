ea9c97119915f81105f4fc20fb8f89a9ad8555fc
Yichen Cai <yc539@cam.ac.uk>
2022-03-02 10:26:06 +0000

    test_plot file created

--- Task2E.py
+++ Task2E.py
@@ -1,1 +1,33 @@
-from floodsystem.
+from floodsystem.plot import plot_water_levels
+from floodsystem.stationdata import build_station_list
+from floodsystem.analysis import update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
+
+def run2e(station, dates, levels):
+    plot_water_levels(station, dates, levels)
+
+if __name__ == "__main__":
+    
+    # Initialization
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    for station in stations:
+        if station.name == "Letcombe Bassett":
+            print(station.name, station.latest_level, station.typical_range,
+            station.relative_water_level())
+    
+    # Top 5 stations with greatest water level
+    risk_stations = stations_highest_rel_level(stations, 5)
+    
+    # Plot past 5 days of levels
+    dt = 10
+    for station in risk_stations:
+        dates, levels = fetch_measure_levels(station.measure_id,
+                                     dt=datetime.timedelta(days=dt))
+        print(levels)
+        run2e(station, dates, levels)
+
+
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,6 +1,7 @@
 from floodsystem.analysis import *
 from datetime import datetime, timedelta
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def plot_water_level_with_fit(station, dates, levels, p):
@@ -28,3 +29,25 @@ def plot_water_level_with_fit(station, dates, levels, p):
     plt.xticks(rotation=45)
     plt.title(station.name)
     plt.show() 
+
+def plot_water_levels(station, dates, levels):
+    """Plot water level data against time for a station
+    with typical low and high levels included
+    label axes
+    station name as plot title"""
+
+    # Plot
+    plt.plot(dates, levels)
+    plt.axhline(y=station.typical_range[0], color='r')
+    plt.axhline(y=station.typical_range[1], color='g')
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45)
+    plt.title("{}".format(station.name))
+
+    # Display plot
+    plt.tight_layout()
+
+    plt.show()
